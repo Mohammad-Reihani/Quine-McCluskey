d131571abee1f4872b51c69944c8ee0154c167d5
Mohammad Reihani <mohammad.reihani82@gmail.com>
2024-05-21 18:27:05 +0330

    initial commit | unfortunataly the first .git was deleted for some reasons, continue from here

---
+++ QuineMcCluskey.h
@@ -0,0 +1,102 @@
+//
+// Created by phoen on 18-May-24.
+//
+
+#ifndef UNTITLED_QUINEMCCLUSKEY_H
+#define UNTITLED_QUINEMCCLUSKEY_H
+
+//#include <iostream>
+
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+
+//this structure stores element of QMC table!!(chi begam akhe!) | so many things are in here, just take a look
+struct GroupedData {
+    std::vector<int> termsIncluded;
+    std::vector<int> deletedArgs;
+    int stage;         //what stage of simplification is it in? actually indicates the column of the table
+    int groupFromTop;  //yeah, for example in initial shit, it starts from 0, 1 (for 1, 2, 4, 8), and so on
+    bool isPI;
+    bool isWaste;
+};
+
+struct PrimeImplicant {
+    std::vector<int> termsIncluded;
+    std::vector<int> deletedArgs;
+    int isEssential;
+    int isRequired;
+};
+
+struct MainTerm {
+    int term;
+    bool isCovered;
+};
+
+class QuineMcCluskey {
+private:
+
+    //TODO Add getStringExpression method and related.
+
+    // Some very important and private variables :
+    bool isSolved = false;
+    int* allTerms{}; //includes dont-care's too
+    int numMainTerms{};
+//    int numDontCares;
+    int numAll{};
+
+    int maxTermIsInput{};
+    int bitsNum{};
+
+    std::vector<MainTerm> MainTerms; // Just mainTerms (minTerms or maxTerms are stored here)
+    std::vector<GroupedData> groupedTerms; //Both mainTerms and don't cares are going to be included here
+    std::vector<PrimeImplicant> primeImplicants; //Both mainTerms and don't cares are going to be included here
+
+
+    // Main functions :
+    void initialMintermsGrouping();
+    void groupMinterms();
+    void markWasteImplicants();
+    void markPrimeImplicants();
+    void extractPrimeImplicants(); //extracts the calculated PI's and then deletes the heavy vector to free up memory
+    void markEssentialPrimeImplicants();
+    void updatedCoveredMainTerms();
+    void ApplyPetricksMethod();
+
+
+    // Sub-functions :
+    int detectBitsCount();
+    void buildMainTerms(int* inputArray);
+    bool uncoveredMainTermsRemained();
+    bool isMainTerm(const int& target);
+
+    // Optional Functions :
+    void printSimplifiedExpression();
+
+    // Static Functions :
+    static int countOnesInBinary(int num);
+    static bool isPowerOfTwo(int num);
+    static bool compareVectors(const std::vector<int>& vec1, const std::vector<int>& vec2, bool considerOrder);
+    static void vectorSelectionSort(std::vector<int>& vec);
+    static bool isSubVector(const std::vector<int>& subVec, const std::vector<int>& mainVec);
+    static bool isSubstringCustom(const std::string& str1, const std::string& str2);
+    static void removeDuplicatesAndSortArray(int* arr, int& size);
+public:
+
+    // Constructors :
+    QuineMcCluskey();
+    QuineMcCluskey(int* mainTermsInputArray, int mainTermsInputArrayLength, bool maxTermsAreInput);//The constructor for NO DON'T CARE'S
+    QuineMcCluskey(int* mainTermsInputArray, int mainTermsInputArrayLength, int* dontCaresInputArray, int dontCaresInputArrayLength, bool maxTermsAreInput);//The constructor for NO DON'T CARE'S
+
+    // Optional : Getters :
+    const std::vector<MainTerm> &getMainTerms() const;
+    const std::vector<GroupedData> &getGroupedTerms() const;
+    const std::vector<PrimeImplicant> &getPrimeImplicants() const;
+
+    void solve();
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "QuineMcCluskey.h"
+using namespace std;
+int main() {
+    int m[11] = {9,12,13,15};
+    int d[7] = {1,4,5,7,8,11,14};
+    int c[12] = {0,1,2,5,6,7};
+    QuineMcCluskey test(m, 4, d, 7, false);
+//    QuineMcCluskey test(c, 6, false);
+    test.solve();
+
+    cout << "Grouped Terms:" << endl;
+    for (const auto& data : test.getGroupedTerms()) {
+        cout << "MainTerms Included: [";
+        for (const auto& minterm : data.termsIncluded) {
+            cout << minterm << " ";
+        }
+        cout << "] Deleted Args: [";
+        for (const auto& arg : data.deletedArgs) {
+            cout << arg << " ";
+
+        }
+//        cout << "]" << endl;
+        cout << "] stage: " << data.stage;
+        cout << " isPI: " << data.isPI <<  endl;
+    }
+
+    cout << "Prime Implicants:" << endl;
+    for (const auto& data : test.getPrimeImplicants()) {
+        cout << "MainTerms Included: [";
+        for (const auto& minterm : data.termsIncluded) {
+            cout << minterm << " ";
+        }
+        cout << "] Deleted Args: [";
+        for (const auto& arg : data.deletedArgs) {
+            cout << arg << " ";
+
+        }
+//        cout << "]" << endl;
+        cout << "] isEssential: " << data.isEssential;
+        cout << " isRequired: " << data.isRequired <<  endl;
+    }
+
+    cout << "*****************************************" << endl << "MainTerms status:" << endl;
+    for (const auto& data : test.getMainTerms()) {
+
+        cout << "term : " << data.term;
+        cout << " | is covered : " << data.isCovered <<  endl;
+    }
+
+    return 0;
+}
