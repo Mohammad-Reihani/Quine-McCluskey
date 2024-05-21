90864601d4970f8060721b27589d102a0877fab0
Mohammad Reihani <mohammad.reihani82@gmail.com>
2024-05-21 20:49:50 +0330

    Checkpoint

--- main.cpp
+++ main.cpp
@@ -1,61 +1,3 @@
-//#include <iostream>
-//
-//
-//using namespace std;
-//
-////This is a demo to how to use QuineMcCluskey Class
-//int main() {
-//    int m[11] = {9,12,13,15};
-//    int d[7] = {1,4,5,7,8,11,14};
-//    int c[12] = {0,1,2,5,6,7};
-////    QuineMcCluskey test(m, 4, d, 7, true);
-//    QuineMcCluskey test(c, 6, false);
-//    test.solve();
-//
-//    cout << "Grouped Terms:" << endl;
-//    for (const auto& data : test.getGroupedTerms()) {
-//        cout << "MainTerms Included: [";
-//        for (const auto& minterm : data.termsIncluded) {
-//            cout << minterm << " ";
-//        }
-//        cout << "] Deleted Args: [";
-//        for (const auto& arg : data.deletedArgs) {
-//            cout << arg << " ";
-//
-//        }
-////        cout << "]" << endl;
-//        cout << "] stage: " << data.stage;
-//        cout << " isPI: " << data.isPI <<  endl;
-//    }
-//
-//    cout << "Prime Implicants:" << endl;
-//    for (const auto& data : test.getPrimeImplicants()) {
-//        cout << "MainTerms Included: [";
-//        for (const auto& minterm : data.termsIncluded) {
-//            cout << minterm << " ";
-//        }
-//        cout << "] Deleted Args: [";
-//        for (const auto& arg : data.deletedArgs) {
-//            cout << arg << " ";
-//
-//        }
-////        cout << "]" << endl;
-//        cout << "] isEssential: " << data.isEssential;
-//        cout << " isRequired: " << data.isRequired <<  endl;
-//    }
-//
-//    cout << "*****************************************" << endl << "MainTerms status:" << endl;
-//    for (const auto& data : test.getMainTerms()) {
-//
-//        cout << "term : " << data.term;
-//        cout << " | is covered : " << data.isCovered <<  endl;
-//    }
-//
-//    cout << "*****************************************" << endl << "here : " << test.getStringExpression() << endl;
-//
-//    return 0;
-//}
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -159,8 +101,6 @@ int main() {
     }
     cout << "-------------------------------------------------------------------" << endl;
     cout << "Simplified Expression : " << qmc.getStringExpression() << endl;//finally showing the simplified expression
-
-    return 0;
 
     return 0;
 }
